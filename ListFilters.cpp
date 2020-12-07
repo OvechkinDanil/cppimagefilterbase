33ce5d927eefd2489c292953f832905c889a252a
Danil Ovechkin <47829271+OvechkinDanil@users.noreply.github.com>
2020-12-07 12:20:35 +0300

    Add files via upload

---
+++ ListFilters.cpp
@@ -0,0 +1,15 @@
+#include "ListFilters.h"
+
+ListOfFilters::ListOfFilters()
+{
+	FilterMap["Red"] = new redFilter();
+}
+
+AbstractFilter* ListOfFilters::Find(std::string filterName)
+{
+	
+	if (FilterMap.find(filterName) != FilterMap.end())
+		return FilterMap.find(filterName)->second;
+	else
+		throw "Filter not found!";
+}
---
+++ config_parsing.cpp
@@ -0,0 +1,60 @@
+#include "config_parsing.h"
+
+ConfigParser::ConfigParser() {};
+
+void ConfigParser::start(std::string ConfigFileName)
+{
+    std::ifstream in(ConfigFileName);
+    if (!in.is_open())
+    {
+        throw "Cannot open file";
+    }
+    else
+    {
+        std::string line;
+        int count = 0;
+        while (getline(in, line))
+        {
+            if (line.length() == 0)
+                continue;
+            FilterData filter;
+            std::istringstream i(line);
+
+            for (std::string str; i >> str;) {
+                if (count == 0)
+                    filter.FilterName = str;
+                else
+                    filter.boarders.push_back(stoi(str));
+                count++;
+            }
+
+            count = 0;
+
+            if (filter.FilterName.length() == 0 || filter.boarders.size() != 4)
+                throw "Not correct config";
+
+            this->ConfigData.push_back(filter);
+        }
+        in.close();
+    }
+}
+
+void ConfigParser::changeBoarders(int h, int w)
+{
+    int tmp;
+    for (int j = 0; j < ConfigData.size(); j++)
+    {
+        for (int i = 0; i < this->ConfigData[j].boarders.size(); i++)
+        {
+            if (this->ConfigData[j].boarders[i] != 0)
+            {
+                tmp = this->ConfigData[j].boarders[i];
+                if (i == 0 || i == 2)
+                    this->ConfigData[j].boarders[i] = h / tmp;
+                else if (i == 1 || i == 3)
+                    this->ConfigData[j].boarders[i] = w / tmp;
+            }
+
+        }
+    }
+}
